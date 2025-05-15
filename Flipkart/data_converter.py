9bf2f500c3e3b490588ff2e3f3fc6b73793fec9f
rushikeshkalbhor20 <kalbhorrushikesh24@gmail.com>
2025-05-15 19:49:56 +0530

    data converter

---
+++ Flipkart/data_converter.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from langchain_core.documents import Document
+
+def dataconverter():
+
+    product_data = pd.read_csv("data\\flipkart_updated.csv")
+
+    data = product_data[["ProductName", "Description"]]
+
+    product_list = []
+
+
+    for index, row in data.iterrows():
+        object = {
+            "ProductName": row["ProductName"],
+            "Description": row["Description"],
+            "Ratings": row["Ratings"],
+            "Prices": row["Prices"]
+        }
+    product_list.append(object)
+    docs = []
+
+    for object in product_list:
+        metadata = {"ProductName": object["ProductName"]}
+        page_content = object["Description"]
+
+        doc = Document(page_content=page_content, metadata=metadata)
+        docs.append(doc)
+    return docs
