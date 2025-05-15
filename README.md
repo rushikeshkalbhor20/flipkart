425b5c845e2288ded824fc070a5da18b01b4470d
rushikeshkalbhor20 <kalbhorrushikesh24@gmail.com>
2025-05-15 19:12:50 +0530

    setting up folder structure

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/rushikeshkalbhor20/flipkart.git
-git push -u origin main
+## Flipkart chat Bot
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements() ->List[str]:
+
+    requirement_list:List[str] = []
+
+    return requirement_list
+
+setup(
+    name = "flipkart",
+    version = "0.0.1",
+    author = "Rushikesh-Kalbhor",
+    author_email="kalbhorrushikesh24@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements()
+
+)
